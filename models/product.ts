7cee35cb019a3a9bfa18f2e068796dc16ee02928
Bruno Ferreira <brunoferreira_@outlook.com>
2023-03-19 11:52:59 -0300

    feat: create product model

--- models/product.ts
+++ models/product.ts
@@ -1,6 +1,6 @@
 import { model, Schema } from "mongoose";
 
-interface Reviews {
+interface Review {
   avatar: string;
   name: string;
   review: string;
@@ -8,24 +8,82 @@ interface Reviews {
 
 interface Product {
   _id: string;
-  image: string;
+  image?: string;
   brand: string;
   price: number;
   rating: null | number;
-  numReviews: null | number;
-  isFeatured: boolean;
+  numReviews?: null | number;
+  isFeatured?: boolean;
   name: string;
   description: string;
   category: {
     $oid: string;
   };
-  reviews: Reviews[];
+  reviews: Review[];
   countInStock: number;
   __v: number;
-  richDescription: string;
-  images: string[];
+  richDescription?: string;
+  images?: string[];
+  dateCreated?: Date;
 }
 
-const productSchema = new Schema<Product>({});
+const productSchema = new Schema<Product>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  brand: {
+    type: String,
+    default: "",
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "category",
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 export const product = model<Product>("products", productSchema);
